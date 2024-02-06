e251d4f81c33b7396be54c8434f6eb908c56de34
Jongmin Han <jonhan@c1r9s5.42seoul.kr>
2024-02-06 11:25:34 +0900

    [feat] : feat cpp_05

---
+++ cpp_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,33 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+    private:
+        const std::string _name;
+        int _grade;
+    public:
+        Bureaucrat(/* args */);
+        ~Bureaucrat();
+
+    class GradeTooHighException : public exception
+    {    
+    private:
+        /* data */
+    public:
+        exception : (/* args */);
+        ~exceptin : ();
+    };    
+    exception : ::exception : (/* args */)
+    {    }
+    
+    exception : ::~exception : ()
+    {    }
+    
+};
+
+
+
+#endif
