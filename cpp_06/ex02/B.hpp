10cbfbb176f83ab0446f03ba079190b2f300dc08
Hanjjong <tim4974@naver.com>
2024-07-17 23:18:00 +0900

    [feat] : feat ex02

---
+++ cpp_06/ex02/B.hpp
@@ -0,0 +1,17 @@
+#ifndef B_HPP
+#define B_HPP
+
+#include "Base.hpp"
+
+class B
+{
+private:
+    /* data */
+public:
+    B(/* args */);
+    ~B();
+};
+
+
+
+#endif
---
+++ cpp_06/ex02/Base.hpp
@@ -0,0 +1,14 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base
+{
+private:
+    /* data */
+public:
+    Base(/* args */);
+    ~Base();
+};
+
+
+#endif
