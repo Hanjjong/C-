3539bc97b59d8fc913993cb49eaa65b7e84e1c24
Jongmin Han <jonhan@c1r8s5.42seoul.kr>
2024-07-18 20:58:55 +0900

    [feat] : finish ex00

---
+++ cpp_07/ex00/whatever.hpp
@@ -0,0 +1,30 @@
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+
+
+template <typename T>
+void swap(T& a, T& b){
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename MIN>
+MIN min(MIN a, MIN b){
+    if (a >= b)
+        return b;
+    return a;
+}
+
+template <typename MAX>
+
+MAX max(MAX a, MAX b){
+    if (a <= b)
+        return b;
+    return a;
+}
+
+#endif
