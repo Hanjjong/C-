2ed236d46b4a20a26cf318ea1f768c64a34051fd
Hanjjong <tim4974@naver.com>
2024-07-21 20:43:13 +0900

    [feat] : feat 07 - ex02

--- cpp_07/ex02/Array.hpp
+++ cpp_07/ex02/Array.hpp
@@ -1,17 +1,62 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <stdexcept>
+
 template <typename T>
 class Array
 {
     private:
-        T elements;
+        T* elements;
+        unsigned int arraySize;
     public:
-        Array();
-        Array(unsigned int n);
-        Array(const Array& obj);
-        Array& operator=(const Array& origin);
-        
+        Array() : elements(0), arraySize(0) {}
+
+        Array(unsigned int n) : arraySize (n){
+            elements = new T[n];
+        }
+
+        Array(const Array& obj) : arraySize(obj.arraySize) {
+            elements = new T[arraySize];
+            for (unsigned int i = 0; i < arraySize; ++i) {
+                elements[i] = obj.elements[i];
+            }
+        }
+
+        Array& operator=(const Array& other) {
+            if (this != &other) {
+                delete[] elements;
+
+                arraySize = other.arraySize;
+                elements = new T[arraySize];
+                for (unsigned int i = 0; i < arraySize; ++i) {
+                    elements[i] = other.elements[i];
+                }
+            }
+            return *this;
+        }
+
+        ~Array(){
+            delete[] elements;
+        }
+
+        T& operator[](unsigned int index) {
+            if (index >= arraySize) {
+                throw std::out_of_range("Index out of bounds");
+            }
+            return elements[index];
+        }
+
+        const T& operator[](unsigned int index) const {
+            if (index >= arraySize) {
+                throw std::out_of_range("Index out of bounds");
+            }
+            return elements[index];
+        }
+
+        unsigned int size() const {
+            return arraySize;
+        }
 };
 
 #endif
---
+++ cpp_07/ex02/main.cpp
@@ -0,0 +1,55 @@
+#include "Array.hpp"
+#include <iostream>
+
+int main() {
+    try {
+        Array<int> arr1;  // 빈 배열 생성
+        Array<int> arr2(5);  // 크기가 5인 배열 생성
+
+        // 배열 요소에 값 할당
+        for (unsigned int i = 0; i < arr2.size(); ++i) {
+            arr2[i] = i * 10;
+        }
+
+        // 복사 생성자 테스트
+        Array<int> arr3 = arr2;
+        arr3[0] = 100;
+
+        std::cout << "arr2: ";
+        for (unsigned int i = 0; i < arr2.size(); ++i) {
+            std::cout << arr2[i] << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "arr3: ";
+        for (unsigned int i = 0; i < arr3.size(); ++i) {
+            std::cout << arr3[i] << " ";
+        }
+        std::cout << std::endl;
+
+        // 대입 연산자 테스트
+        Array<int> arr4;
+        arr4 = arr2;
+        arr4[1] = 200;
+
+        std::cout << "arr2: ";
+        for (unsigned int i = 0; i < arr2.size(); ++i) {
+            std::cout << arr2[i] << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "arr4: ";
+        for (unsigned int i = 0; i < arr4.size(); ++i) {
+            std::cout << arr4[i] << " ";
+        }
+        std::cout << std::endl;
+
+        // 예외 처리 테스트
+        std::cout << arr2[10] << std::endl;  // 인덱스 초과 예외 발생
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
