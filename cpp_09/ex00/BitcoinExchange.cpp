dd5d3fc5cd4851b2e16f239aa243d32a114dfc25
Jongmin Han <jonhan@c1r8s1.42seoul.kr>
2024-08-18 22:38:32 +0900

    [feat] : finish ex00

--- cpp_09/ex00/BitcoinExchange.cpp
+++ cpp_09/ex00/BitcoinExchange.cpp
@@ -1,8 +1,30 @@
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::BitcoinExchange(std::string fname)
-{
-    loadDataBase(fname);
+
+BitcoinExchange::BitcoinExchange(std::string fname){loadDataBase(fname);}
+
+BitcoinExchange::~BitcoinExchange() {}
+
+static std::string trim(const std::string& str) {
+    std::string::size_type start = 0;
+    while (start < str.size() && std::isspace(str[start])) {
+        ++start;
+    }
+    std::string::size_type end = str.size();
+    while (end > start && std::isspace(str[end - 1])) {
+        --end;
+    }
+    return str.substr(start, end - start);
+}
+
+static bool isLeapYear(int year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            return year % 400 == 0;
+        }
+        return true;
+    }
+    return false;
 }
 
 void BitcoinExchange::loadDataBase(std::string fname){
@@ -16,7 +38,7 @@ void BitcoinExchange::loadDataBase(std::string fname){
         std::string date;
         float rate;
         if (std::getline(ss, date, ',') && ss >> rate){
-            bitcoinData[date] = rate;
+            bitcoinData[trim(date)] = rate;
         }
     }
     // bitcoinData 맵 출력
@@ -29,21 +51,65 @@ void BitcoinExchange::loadDataBase(std::string fname){
 
 void BitcoinExchange::is_valid_date(std::string date)
 {
-    if (date.length() != 11){
-        throw std::runtime_error("Error: Invalid input date");
+    int year = 0;
+    int month = 0;
+    int day = 0;
+
+    if (date.length() != 10){
+        throw std::runtime_error("Error: bad input => " + date);
     }
+    // check year;
+    std::stringstream ss;
     int i = 0;
-    while (date[i]){
-        // - 위치 , 윤년 확인
+    while (date[i] != '-'){
+        if (!std::isdigit(date[i]))
+            throw std::runtime_error("Error: bad input => " + date);
+        ss << date[i];
         i++;
+    }
+    ss >> year;
+    ss.clear();
+    if (i != 4 || year < 2009 || date[i] != '-')
+        throw std::runtime_error("Error: bad input => " + date);
+    i++;
+
+    // check month;
+    while (date[i] != '-'){
+        if (!std::isdigit(date[i]))
+            throw std::runtime_error("Error: bad input => " + date);
+        ss << date[i];
+        i++;    
+    }
+    ss >> month;
+    ss.clear();
+    if (i != 7 || month < 1 || month > 12 || date[i] != '-')
+        throw std::runtime_error("Error: bad input => " + date);
+    i++;    
+
+    //check day;
+    while (date[i]){
+        if (!std::isdigit(date[i]))
+            throw std::runtime_error("Error: bad input => " + date);
+        ss << date[i];
+        i++;    
+    }
+    ss >> day;
+    int daysInMonth[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    // 윤년일 경우 2월의 일자 조정
+    if (isLeapYear(year))
+        daysInMonth[1] = 29;
+    if (day < 1 || day > daysInMonth[month - 1]) {
+        throw std::runtime_error("Error: bad input => " + date);
     }
 }
 
-double BitcoinExchange::getExchangeRate(std::string date)
+float BitcoinExchange::getExchangeRate(std::string date)
 {
     std::string closestDate = findClosestDate(date);
+    if (closestDate == "")
+        throw std::runtime_error("해당하는 날짜의 환율이 없습니다...");
 
-    return 0.0;
+    return bitcoinData[closestDate];
 }
 
 std::string BitcoinExchange::findClosestDate(std::string date)
@@ -57,23 +123,26 @@ std::string BitcoinExchange::findClosestDate(std::string date)
 
     // 주어진 날짜보다 이후의 날짜를 가리키는 경우
     if (it != bitcoinData.begin()) {
-        --it; // 이전 날짜로 이동
-        return it->first;
+    --it; // 이전 날짜로 이동
+    return it->first;
     }
 
     // 모든 날짜가 주어진 날짜보다 이후인 경우
     return "";
 }
 
-double BitcoinExchange::is_valid_value(std::string input)
+float BitcoinExchange::is_valid_value(std::string input)
 {
     char* endptr;
-    double rate = std::strtod(input.c_str(), &endptr);
+    float rate = std::strtof(input.c_str(), &endptr);
     
     // 변환이 실패한 경우, endptr이 input의 시작 주소와 같음.
-    if (input.c_str() == endptr || rate < 0) {
-        throw std::runtime_error("Error: Invalid input rate");
-    }
+    if (input.c_str() == endptr)
+        throw std::runtime_error("Error: Rate value doesn't exist");
+    if (rate < 0) 
+        throw std::runtime_error("Error: not a positive number.");
+    if (rate > 1000)
+        throw std::runtime_error("Error: too large a number.");
     return rate;
 }
 
@@ -82,38 +151,40 @@ void BitcoinExchange::processInput(std::string fname){
     if (!file.is_open()){
         throw std::runtime_error("Error: could not open input data");
     }
-
     std::string line;
     std::getline(file, line);
     if (line != "data | value")
         throw std::runtime_error("Invalid data fomat");
+
     while (std::getline(file, line)){
+        if (line.empty())
+            continue;
         std::stringstream ss(line);
         std::string date;
         std::string rate;
-        if (std::getline(ss, date, '|') && ss >> rate){
-            int valid_value;
-            try{
+        if (std::getline(ss, date, '|')) {
+            date = trim(date); 
+            if (line.empty())
+                continue;
+            std::getline(ss, rate);
+            rate = trim(rate); 
+            try {
+                // 날짜 및 값 유효성 검사
                 is_valid_date(date);
-            }
-            catch (const std::exception& e){
-                std::cerr << e.what() << std::endl;
-            }
-            try{
-                valid_value = is_valid_value(rate);
-            }
-            catch (const std::exception& e){
-                std::cerr << e.what() << std::endl;
-            }
+                float valid_value = is_valid_value(rate);
 
-            int rate = getExchangeRate(date);
-            inputData[date] = valid_value;
-            std::cout << date << "=> " << inputData[date] << std::endl;
+                // 환율 가져오기 및 계산
+                float exchangeRate = getExchangeRate(date);
+                inputData[date] = valid_value;
+                
+                std::cout << date << " => " << inputData[date] << " = " << exchangeRate * inputData[date] << std::endl;
+            } catch (const std::exception& e) {
+                std::cerr << e.what() << std::endl;
+            }
+        } else {
+            std::cerr << "Error: bad input => " << line << std::endl;
         }
     }
 }
 
 
-BitcoinExchange::~BitcoinExchange()
-{
-}
--- cpp_09/ex00/BitcoinExchange.hpp
+++ cpp_09/ex00/BitcoinExchange.hpp
@@ -13,8 +13,8 @@ private:
     std::map<std::string, float> inputData;
     void loadDataBase(std::string fname);
     void is_valid_date(std::string date);
-    double is_valid_value(std::string rate);
-    double getExchangeRate(std::string date);
+    float is_valid_value(std::string rate);
+    float getExchangeRate(std::string date);
     std::string findClosestDate(std::string date);
 public:
     BitcoinExchange(std::string fname);
@@ -22,6 +22,7 @@ public:
     void processInput(std::string fname);
 };
 
-
+static std::string trim(const std::string& str);
+static bool isLeapYear(int year);
 
 #endif
